47a14d9e6a12d8fbb4be08ea4d52a322944abb6c
James1056 <copetakejames@outlook.com>
2023-03-15 15:43:19 -0400

    all files added

---
+++ src/main/java/com/csc340sp23/bookmanager/BookService.java
@@ -0,0 +1,29 @@
+package com.csc340sp23.bookmanager;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository repo;
+
+    public List<Book> getAllBooks() {
+        return repo.findAll();
+    }
+   
+    public Book getBook(long id) {
+        return repo.getReferenceById(id);
+    }
+
+    public void deleteBook(long id) {
+        repo.deleteById(id);
+    }
+
+    void saveBook(Book book) {
+        repo.save(book);
+    }
+    
+}
